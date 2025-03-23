5706eb817d813bd137cca4bbded5a4785fbd9582
hwan04 <73435826+hwan04@users.noreply.github.com>
2025-03-24 04:02:19 +0900

    feat(SplashActivity): Splash 화면 제작

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.freemates_android"
-        minSdk = 24
+        minSdk = 31
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -49,4 +49,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.androidx.core.splashscreen)
 }
