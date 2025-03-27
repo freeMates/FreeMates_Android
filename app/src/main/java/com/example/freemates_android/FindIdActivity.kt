0618dbab02a892e6f091813b906164d75f652747
hwan04 <73435826+hwan04@users.noreply.github.com>
2025-03-27 10:24:42 +0900

    feat(FindIdActivity): 기능 구현

--- app/src/main/java/com/example/freemates_android/FindIdActivity.kt
+++ app/src/main/java/com/example/freemates_android/FindIdActivity.kt
@@ -2,15 +2,19 @@ package com.example.freemates_android
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 import com.example.freemates_android.databinding.ActivityFindIdBinding
 import com.example.freemates_android.databinding.ActivityLoginBinding
 
 class FindIdActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFindIdBinding
+    private var isPhoneVerified = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,6 +32,23 @@ class FindIdActivity : AppCompatActivity() {
             finish()
         }
 
+        binding.etUserPhoneFindId.addTextChangedListener {
+            binding.btnVerificationCodeCheckFindId.isSelected = false
+        }
+
+        binding.btnUserPhoneVerifyFindId.setOnClickListener {
+            submitUserPhoneVerify()
+            isPhoneVerified = false
+        }
+
+        binding.btnVerificationCodeCheckFindId.setOnClickListener {
+            if(binding.btnVerificationCodeCheckFindId.isSelected) {
+                submitUserVerificationCode()
+            }
+            else
+                showToast("인증하기 버튼을 눌러주세요")
+        }
+
         binding.tvChangePasswordFindId.setOnClickListener {
             val intent = Intent(this, ChangePasswordActivity::class.java)
             startActivity(intent)
@@ -41,9 +62,59 @@ class FindIdActivity : AppCompatActivity() {
         }
 
         binding.btnCompleteFindIdFindId.setOnClickListener {
+            submitFindId()
+        }
+    }
+
+    private fun submitUserPhoneVerify(){
+        val phone = binding.etUserPhoneFindId.text.toString().trim()
+
+        if (!phone.matches(Regex("^01[0-9]{8,9}$"))) {
+            showToast("올바른 전화번호 형식을 입력해주세요.")
+            binding.btnVerificationCodeCheckFindId.isSelected = false
+            return
+        }
+
+        // TODO : API 호출
+
+        binding.tvVerificationCodeErrorFindId.visibility = View.INVISIBLE
+        binding.btnVerificationCodeCheckFindId.isSelected = true
+
+        showToast("인증번호를 전송했습니다.")
+    }
+
+    private fun submitUserVerificationCode(){
+        // TODO : API 호출
+
+        // TODO : 나중에 값 변경!
+        val state = true
+
+        isPhoneVerified = state
+
+        if(isPhoneVerified)
+            binding.tvVerificationCodeErrorFindId.visibility = View.INVISIBLE
+        else
+            binding.tvVerificationCodeErrorFindId.visibility = View.VISIBLE
+
+        updateRegisterButtonState()
+    }
+
+    private fun updateRegisterButtonState() {
+        val isReady = isPhoneVerified
+        binding.btnCompleteFindIdFindId.isSelected = isReady
+    }
+
+    private fun submitFindId(){
+        if(binding.btnCompleteFindIdFindId.isSelected) {
+            // TODO : 회원가입 API 호출
+
             val intent = Intent(this, FindIdCompleteActivity::class.java)
             startActivity(intent)
             finish()
         }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
